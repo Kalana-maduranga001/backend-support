c334680cee02d39a7b463383b223942779af5088
Kalana Maduranga <kalanamadu2000@gmail.com>
2026-01-05 01:08:19 +0530

    add controler Updated

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,5 +3,15 @@ const bcrypt = require("bcryptjs");
 
 // Register User
 exports.register = async(req , res) => {
-    
+    try{
+        const {name , email , password} = req.body;
+
+        if(!name || !email || !password){
+            return res.status(400).json({message: "Please field are required"});
+        }
+
+    }catch(error){
+        console.error("Registration error:", error.message);
+        res.status(500).json({ message: "Server Error" });
+    }
 }
