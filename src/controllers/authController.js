f98f4a3f7ab949282c00b52829635c8b7f97ffa2
Kalana Maduranga <kalanamadu2000@gmail.com>
2026-01-05 01:12:21 +0530

    add controler Updated

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,11 +5,16 @@ const bcrypt = require("bcryptjs");
 exports.register = async(req , res) => {
     try{
         const {name , email , password} = req.body;
-
+        //validation
         if(!name || !email || !password){
             return res.status(400).json({message: "Please field are required"});
         }
-
+        //check esisting user
+        const existingUser = await User.findOne({ email });
+        if(existingUser){
+            return res.status(400).json({message: "User alredy exsists"});
+        }
+        //hash password
     }catch(error){
         console.error("Registration error:", error.message);
         res.status(500).json({ message: "Server Error" });
