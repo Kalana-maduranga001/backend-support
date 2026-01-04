2b56053e018d1338b9f2afd03f28e7db8c4bfe56
Kalana Maduranga <kalanamadu2000@gmail.com>
2026-01-05 01:19:18 +0530

    add controler Updated

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -18,6 +18,20 @@ exports.register = async(req , res) => {
         const salt = await bcrypt.getSalt(10);
         const hashedPassword = await bcrypt.hash(password , salt);
         //create user
+        const user = await User.create({
+            name, 
+            email,
+            password: hashedPassword,
+        });
+        res.status(201).json({
+            message: "User registered successfully",
+            user:{
+                id:user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role,
+            },
+        });
     }catch(error){
         console.error("Registration error:", error.message);
         res.status(500).json({ message: "Server Error" });
