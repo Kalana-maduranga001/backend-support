d0b8b4becbece778d94f919bdc78e93a3b792221
Kalana Maduranga <kalanamadu2000@gmail.com>
2026-01-05 18:49:47 +0530

    login function buliding

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -37,14 +37,23 @@ exports.register = async (req, res) => {
 exports.login = async (req , res) => {
    const {email , password} = req.body;
    try{
-       const userEmail = await User.findOne({email});
-        if(!userEmail){
+       const user = await User.findOne({email});
+        if(!user){
             return res.status(400).json({message: "Invalid Email."});
         }
-        const isMatch = await bcrypt.compare(password , userEmail.password);
+        const isMatch = await bcrypt.compare(password , user.password);
         if(!isMatch){
             return res.status(400).json({message: "Invalid Password."});
         }
+
+        const token = jwt.sign(
+            {id: user._id, role: user.role},
+            process.env.JWT_SECRET,
+            {expiresIn: "7d"}
+        );
+
+        res.status(200).json({message:"Login Successful", token});
+        
     }catch(error){
         res.status(500).json({message:"Server Error"});
     }
